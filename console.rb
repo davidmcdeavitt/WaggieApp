438d1c01db9c53a65a7c0b098721d6720ae5b666
David McDeavitt <davidmcdeavitt@192-168-1-4.tpgi.com.au>
2017-07-04 19:57:03 +1000

    update tuesday night

---
+++ console.rb
@@ -0,0 +1,7 @@
+require_relative 'db_config'
+require_relative 'models/user'
+require 'pry'
+
+
+
+binding.pry
--- main.rb
+++ main.rb
@@ -31,15 +31,33 @@ end
 enable :sessions
 
 get '/' do
-  redirect '/login'
+  erb :main
+end
+
+post '/go_login' do
+  redirect 'login'
+end
+
+post '/create_new_form' do
+  erb :new_user
 end
 
 get '/login' do
   erb :login
 end
 
+
 post '/session' do
   user = User.find_by(email:params[:email])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect '/dashboard'
+  else
+    erb :login
+  end
+end
+post '/create_new' do
+  user = User.find_by(email: params[:email])
   if user && user.authenticate(params[:password])
     session[:user_id] = user.id
     redirect '/dashboard'
